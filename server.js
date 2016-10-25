ffab50fc5f7104c6ff6bd907b176c20aa715a604
Julian Mazzitelli <mazzitelli.julian@gmail.com>
2016-10-25 19:59:49 -0400

    signup endpoint

--- server.js
+++ server.js
@@ -16,6 +16,30 @@ globalMiddlewares.map(mw => app.use(mw))
 // Apply routes
 app.get('/', (req, res) => res.send('Hello world\n'))
 
+app.post('/signup', (req, res) => {
+  const Promise = require('bluebird')
+  const bcryptjs = require('bcryptjs')
+  const bcrypt = {
+    hash: Promise.promisify(bcryptjs.hash),
+    genSalt: Promise.promisify(bcryptjs.genSalt)
+  }
+  const { Users } = require('models/index.js')
+
+  const { email, password } = req.body
+
+  bcrypt.genSalt(10)
+    .then(salt => bcrypt.hash(password, salt))
+    .then(hash => Users.create({ email, password: hash }))
+    .then(() => res.send({
+      success: true,
+      message: `User account ${email} created`
+    }))
+    .catch(err => {
+      console.error(err)
+      res.status(500)
+    })
+})
+
 // Listen once everything ready
 Promise.all([
   initializeDb()
